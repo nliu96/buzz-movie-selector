a553c26a286c50e06f6bf53ed763c80ee5f45238
Nick Liu <nliu41@gatech.edu>
2015-06-17 11:55:11 -0400

    editting

---
+++ src/java/Movie.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author theaz_000
+ */
+public class Movie {
+    private int id;
+    private String title;
+    private int year;
+    private List<String> genres;
+    private String mpaa_rating;
+    
+}
