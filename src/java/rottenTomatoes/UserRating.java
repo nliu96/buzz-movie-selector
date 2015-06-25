300f9e7d51054ce9b61af03e0c7338218054f754
Nick Liu <nliu41@gatech.edu>
2015-06-25 01:57:20 -0400

    Added rating input, working on saving and multiple ratings

---
+++ src/java/rottenTomatoes/UserRating.java
@@ -0,0 +1,38 @@
+package rottenTomatoes;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author theaz_000
+ */
+public class UserRating {
+    private int rating;
+    private UserData user;
+    private String major;
+    
+    public UserRating(int rating, UserData user) {
+        this.rating = rating;
+        this.user = user;
+        major = user.getMajor();
+    }   
+
+    public int getRating() {
+        return rating;
+    }
+
+    public UserData getUser() {
+        return user;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
